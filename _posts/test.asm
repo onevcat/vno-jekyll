29bd8e34723e3b507288571f8afe3310e438fc7a
webzququ <qzh813@163.com>
2019-12-07 11:49:58 +0800

    new post

--- _posts/test.asm
+++ _posts/test.asm
@@ -1,10 +1,27 @@
 assume cs:codesg
-  codesg segment
-    start:mov ax, 0
-          mov bx, 0
-          jump far ptr s
-          db 256 dup(0)
-        s:add ax, 1
-          inc ax
-  codesg ends
+codesg segment
+
+       mov ax, 4c00h
+       int 21h
+
+start: mov ax, 0
+    s: nop
+       nop
+
+       mov di, offset s
+       mov si, offset s2
+       mov ax, cs:[si]
+       mov cs:[di], ax
+
+   s0: jmp short s
+
+   s1: mov ax, 0
+       int 21h
+       mov ax, 0
+
+   s2: jmp short s1
+       nop
+
+codesg ends
 end start
+```
